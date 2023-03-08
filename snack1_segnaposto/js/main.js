1269ee0fa293fd1cbfa9d45133118f444669f66b
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-08 17:43:24 +0100

    Snack 1 - ciclo MAP -aggiunta Oggetti-segnaposto

--- snack1_segnaposto/js/main.js
+++ snack1_segnaposto/js/main.js
@@ -6,20 +6,40 @@ const startList = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo',
 // Verifica da console dell'Array iniziale
 console.log('Array di partenza degli invitati', startList);
 
-// Lancio un ciclo MAP sull'array iniziale per clonarlo in un nuovo Array
+// Lanciare un ciclo MAP sull'array iniziale per clonarlo in un nuovo Array
 const newList = startList.map((element) => element);
 
 // Rimuovere dal nuovo Array l'ultimo invitato e aggiungere i 4 Maneskin
 newList.pop(); //rimuove l'ultimo elemento dell'array
 newList.push('Damiano David', 'Victoria De Angelis', 'Thomas Raggi', 'Ethan Torchio');
 
-// Stampo in console la newList invitati aggiornata
+// Stampare in console la newList invitati aggiornata
 console.log('Nuovo Array degli invitati', newList);
 
-// Creo un Array vuoto che dovrà contenere i miei oggetti finali (segnaposto)
+// SOLUZIONE A:
+/* Avviare un ciclo MAP che, partendo dall'Array newList, prende l'elemento
+e l'indice di posizionamento e crea per ogni elemento dell'Array un oggetto
+contenente il nome, il posto, il nome_tavolo.*/
+const finalList = newList.map((element, index) => {
+       return{
+           nome: element,
+           posto: index + 1,
+           nome_tavolo: 'Tavolo Vip',
+       }
+ })
+
+// Stampare in console la lista finale OBJECTS (segnaposto)
+console.log('Lista finale OGGETTI', finalList);
+
+
+// ----------------------------------------------------------
+// SOLUZIONE B (ALTERNATIVA - commentata - testata e funzionante)
+/* Creare un Array vuoto finalList che dovrà contenere i miei oggetti 
+finali (segnaposto)
 const finalList = [];
-/*Lanciare un ciclo FOR-EACH per passare la NewList ed
-includere gli oggetti (nome, posto, nometavolo) nell'array finale*/
+
+Lanciare un ciclo FOR-EACH per passare la NewList ed
+includere gli oggetti (nome, posto, nometavolo) nell'array finale
 newList.forEach((element, index) =>{
    finalList.push(
         {
@@ -28,13 +48,9 @@ newList.forEach((element, index) =>{
         nome_tavolo: 'Tavolo Vip',
         }
     );
+})
 
-}
-)
-
-console.log('Lista finale OGGETTI', finalList);
-
-
+console.log('Lista finale OGGETTI', finalList);*/
 
 
 
