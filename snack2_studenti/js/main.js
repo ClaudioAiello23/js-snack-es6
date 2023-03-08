445052426bbe3c4ee772d8a186af8395835da1eb
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-08 18:13:30 +0100

    snack2: inserimento array oggetti iniziale

--- snack2_studenti/js/main.js
+++ snack2_studenti/js/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// Creare l'Array inziale di Objects studenti
+const studentStartList = [
+    {
+        id: 213,
+        nome: 'Marco della Rovere',
+        somma_voti: 78,
+    },
+    {
+        id: 110,
+        nome: 'Paola Cortellessa',
+        somma_voti: 96,
+    },
+    {
+        id: 250,
+        nome: 'Andrea Mantegna',
+        somma_voti: 48,
+    },
+    {
+        id: 145,
+        nome: 'Gaia Borromini',
+        somma_voti: 74,
+    },
+    {
+        id: 196,
+        nome: 'Luigi Grimaldello',
+        somma_voti: 68,
+    },
+    {
+        id: 102,
+        nome: 'Piero della Francesca',
+        somma_voti: 50,
+    },
+    {
+        id: 120,
+        nome: 'Francesca da Polenta',
+        somma_voti: 84,
+    },
+];
+
+console.log('Lista iniziale studenti: ', studentStartList);
+
