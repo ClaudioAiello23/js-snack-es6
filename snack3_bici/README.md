b37713fc88f5562388f1c60286704a4d0981cb4f
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-09 21:11:53 +0100

    Snack3: sistemazione README e file JS

--- snack3_bici/README.md
+++ snack3_bici/README.md
@@ -5,4 +5,7 @@ Stampare in console la bici con peso minore utilizzando destructuring e template
 ----------------------------------------------------------------------------------------
 SVOLGIMENTO ESERCIZIO - IPOTESI
 - Creare l'array contenente gli oggetti 'BICI DA CORSA' aventi le proprietà (chiavi + valore) nome e peso.
-- Individuare ed estrapolare dall'Array di partenza l'oggetto avente la chiave PESO con valore più basso, utilizzando il Destructuring e il template literal.
+- Individuare ed estrapolare dall'Array di partenza l'oggetto avente la chiave PESO con valore più basso; si potrebbe utilizzare il posizionamento oggetto dell'Array per individuare l'oggetto bici con peso più basso.
+- Utilizzando il Destructuring, estrapolare dall'oggetto selezionato i valori del nome
+bici e del peso bici, saalvandoli nelle relative variabili.
+- Utilizzando il template literal, stampare in console.log una frase finale che richiama la bici più leggera, citandone la marca (nome) ed il peso (peso_kg).
--- snack3_bici/js/main.js
+++ snack3_bici/js/main.js
@@ -1,1 +1,68 @@
 'use strict';
+
+
+// Creare l'Array di oggetti BICI DA CORSA
+const bikeList = [
+    {
+        nome: 'bianchi',
+        peso_kg: 9.7,
+
+    },
+    {
+        nome: 'atala',
+        peso_kg: 9.1,
+
+    },
+    {
+        nome: 'olmo',
+        peso_kg: 10.2,
+
+    },
+    {
+        nome: 'casati',
+        peso_kg: 7.9,
+
+    },
+    {
+        nome: 'colnago',
+        peso_kg: 9.3,
+
+    },
+    {
+        nome: 'bottecchia',
+        peso_kg: 9.9,
+
+    },
+    {
+        nome: 'cinelli',
+        peso_kg: 8.4,
+
+    },
+    {
+        nome: 'pistidda',
+        peso_kg: 8.6,
+
+    }
+];
+
+// Stampare da console l'intero Array di oggetti
+console.log(bikeList);
+
+// Stampare da console l'oggetto bici con peso più basso
+console.log(bikeList[3]);
+
+/*Destrutturare l'oggetto bici selezionato - DESTRUCTURING
+NB alle chiavi oggetto "nome" e "peso_kg" della bici selezionata
+attribuisco uno nome di variabile (per il nome uso bikeSelectedName,
+per il peso bikeSelectedWeight)*/
+const {nome: bikeSelectedName, peso_kg: bikeSelectedWeight} = bikeList[3];
+
+// stampo in console le due variabili appena create tramite il DESTRUCTURING
+console.log (bikeSelectedName, bikeSelectedWeight);
+
+// stampo con template literal la frase finale
+console.log(`Partendo dalla selezione iniziale di bici da corsa, la bici più leggera appartiene alla marca ${bikeSelectedName.toUpperCase()} e ha un peso di ${bikeSelectedWeight} Kg.`);
+
+
+
+
